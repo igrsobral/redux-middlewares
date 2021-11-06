a4d1e557126e7a4141d86d72aca1c09f33ec8511
igor.sobral <igor.sobral@pagtel.com.br>
2021-11-06 15:15:05 -0300

    chore: add logger middleware and config in store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,18 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { logger } from './middlewares/logger';
+
+
+const customMiddleware = [logger];
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: (getDefaultMiddleware) => [
+    ...getDefaultMiddleware(),
+    ...customMiddleware,
+  ],
 });
 
 export type AppDispatch = typeof store.dispatch;
